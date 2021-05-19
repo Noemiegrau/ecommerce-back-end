f2ce42544fc5a45b1b859c38d3f263d0d0ca18d8
Noemiegrau <noemiegrau@outlook.fr>
2021-05-19 12:21:40 -0700

    fixed product delete route error

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -128,7 +128,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  Category.destroy({
+  Product.destroy({
     where: {
       id: req.params.id
     }
